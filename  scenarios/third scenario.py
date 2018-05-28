08d05fa37b7a48f406b6749abc922c681f47117e
al-ani <ayymn@yahoo.com>
2018-05-27 19:05:55 -0700

    Add files via upload

---
+++  scenarios/third scenario.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+import sys
+
+ip=IPv6(src="fe80::200:ff:fe00:1", dst="fe80::200:ff:fe00:2")
+icmp=ICMPv6EchoRequest()
+F=IPv6ExtHdrFragment()
+op=[Pad1()]
+for i in range(0,1300): op=op+[Pad1()]
+D=IPv6ExtHdrDestOpt(options=op)
+
+p=(ip/F/D/D/icmp)
+fs=fragment6(p,1280)
+send(fs)
