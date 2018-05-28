ad50913a34a9eeb94df4e7012c0e88a737194986
al-ani <ayymn@yahoo.com>
2018-05-27 19:14:43 -0700

    Add files via upload

---
+++ Untitled Folder/ofsoftswitch13/third sceanrio.py
@@ -0,0 +1,15 @@
+
+from scapy.all import *
+import sys
+
+ip=IPv6(src="fe80::200:ff:fe00:1", dst="fe80::200:ff:fe00:2")
+tcp=TCP()
+F=IPv6ExtHdrFragment()
+op=[Pad1()]
+for i in range(0,1300): op=op+[Pad1()]
+pad=[Pad1(),Pad1(),Pad1(),Pad1(),Pad1(),Pad1()]
+D=IPv6ExtHdrDestOpt(options=op)
+
+p=(ip/F/D/tcp)
+fs=fragment6(p,1280)
+send(fs)
